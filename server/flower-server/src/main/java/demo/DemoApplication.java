0eb22a25e8382814105e7ff9a4e88407952b225f
stormning <stormning@163.com>
2015-07-22 15:50:04 +0800

    基本结构

---
+++ server/flower-server/src/main/java/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+/*
+ * Project:  flower
+ * Module:   flower-server
+ * File:     DemoApplication.java
+ * Modifier: nzhou
+ * Modified: 2015-07-22 06:13
+ * Copyright (c) 2014 Wisorg All Rights Reserved.
+ * Copying of this document or code and giving it to others and the
+ * use or communication of the contents thereof, are forbidden without
+ * expressed authority. Offenders are liable to the payment of damages.
+ * All rights reserved in the event of the grant of a invention patent
+ * or the registration of a utility model, design or code.
+ */
+
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
