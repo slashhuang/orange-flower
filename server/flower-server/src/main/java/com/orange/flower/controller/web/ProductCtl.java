3afbf8656eff993fb094dbc1d7a7bc92c3c649e8
stormning <stormning@163.com>
2015-07-27 14:32:01 +0800

    controllers

---
+++ server/flower-server/src/main/java/com/orange/flower/controller/web/ProductCtl.java
@@ -0,0 +1,25 @@
+/*
+ * Project:  flower
+ * Module:   flower-server
+ * File:     ProductCtl.java
+ * Modifier: nzhou
+ * Modified: 2015-07-27 14:31
+ * Copyright (c) 2014 Wisorg All Rights Reserved.
+ * Copying of this document or code and giving it to others and the
+ * use or communication of the contents thereof, are forbidden without
+ * expressed authority. Offenders are liable to the payment of damages.
+ * All rights reserved in the event of the grant of a invention patent
+ * or the registration of a utility model, design or code.
+ */
+
+package com.orange.flower.controller.web;
+
+/**
+ * .
+ * <p/>
+ *
+ * @author <a href="mailto:stormning@163.com">stormning</a>
+ * @version V1.0, 2015/7/27
+ */
+public class ProductCtl {
+}
