75cd13f4d3d617b35d3faab5a77bb24a6ca84987
slashhuang <627284482@qq.com>
2015-08-18 21:23:19 +0800

    adding htmls and effect

--- public/js/bill/bill.js
+++ public/js/bill/bill.js
@@ -36,14 +36,65 @@ require.config({
 
 define(["zepto","sweetalert","util/url_config"], function($,sweet,config){
     var payBillNOW = $("#payBillNOW");
+    var billMonthFilter = $("#billMonthFilter");//账单月份遮罩层
+    (function(){
+        var setFilterHeight =$("#billHeaderHeight").offset().height;
+        billMonthFilter.css({
+            "position":"absolute",
+            "z-index":"1000",
+            "top":setFilterHeight
+        });
+    })();
+
+    //设置最小高度
     (function(){
         var setMinHeight = window.screen.height + "px";
         document.getElementsByClassName("of-bill-bg")[0].style.minHeight=setMinHeight;
     })();
+    //付款按钮
     payBillNOW.on({
         "tap":function(){
             window.location.href=""
         }
     });
+    //点击月份显示数据
+    var monthSelector = $(".of-bill-month-list-box").find("li");
+    var billHeaderHeight = $("#billHeaderHeight");//头部点击隐藏
+    monthSelector.on({
+        "tap":function(e){
+            var target = e.target|| e.srcElement;
+            if(target.tagName="i"){
+              monthSelector.find("i").removeClass("active");
+              target.classList.add("active");
+                slider()
+            }
+        }
+    });
+    var status = 0;
+    var slider = function(){
+        var settings={
+            "height":"0",
+            "overflow":"hidden"
+        };
+        console.log(billMonthFilter);
+        switch(status){
+            case 0:
+                billMonthFilter.animate(settings,100);
+                status =1;
+                break;
+            case 1:{
+                billMonthFilter.animate({"height":"","overflow":""},100);
+                status =0;
+                break;
+            }
+        }
+
+    }
+    billHeaderHeight.on({
+        "tap": slider
+    })
+
+
+
 
 });
