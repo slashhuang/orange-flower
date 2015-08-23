5462cb839c7a8731d321a6cbe89e59c64472f807
Mrson <rw_Song@sina.com>
2015-08-23 20:49:07 +0800

    账单同步完成

--- js/controller/bill/billMainCtrl.js
+++ js/controller/bill/billMainCtrl.js
@@ -10,6 +10,79 @@ define([], function () {
         $scope.month = date.getMonth() + 1;
         //  取得当前年月
 
+        $scope.handle = false;
+        //  判断是否隐藏显示
+
+        $http({
+            "method":"post",
+            "url": prefuri + "/repay/bill/{year}/{month}?month=" + $scope.month + "&year=" + $scope.year
+        }).success(function(res){
+            $scope.data = res;
+        }).error(function(){
+        });
+
+        /**
+         * 渲染新的信息DOM
+         * @param month
+         */
+        $scope.rendNewInfo = function(month){
+            $scope.month = month;
+            $http({
+                "method":"post",
+                "url": prefuri + "/repay/bill/{year}/{month}?month=" + $scope.month + "&year=" + $scope.year
+            }).success(function(res){
+                $scope.data = res;
+                $scope.handle = false;
+            }).error(function(){
+            });
+        };
+
+        /**
+         * 下月按钮
+         */
+        $scope.nextMonth = function(){
+            $scope.month ++;
+            if($scope.month > 12){
+                $scope.month = 1;
+                $scope.year ++;
+            }
+        };
+
+        $scope.lastMonth = function(){
+            $scope.month --;
+            if($scope.month < 1){
+                $scope.month = 12;
+                $scope.year --;
+            }
+        };
+
+        /**
+         * 将价格转换成两位有效数字
+         * @param price
+         * @returns {string}
+         */
+        $scope.transferPrice = function(price){
+            return (price / 100).toFixed(2);
+        };
+
+        /**
+         * 操作隐藏和显示
+         */
+        $scope.toggleShow = function(){
+            $scope.handle = !$scope.handle;
+        };
+
+        /**
+         * 当前月的选中效果
+         * @param month
+         * @returns {string}
+         */
+        $scope.active = function(month){
+            if(month == $scope.month){
+                return "active";
+            }
+            return "";
+        };
 
     };
     billMainCtrl.$inject = ['$scope', '$routeParams', '$location', '$http'];
