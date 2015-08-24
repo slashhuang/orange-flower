52cfa04b77fcc18b6a9e11275e8214bc13af26d1
slashhuang <627284482@qq.com>
2015-08-24 12:54:02 +0800

    revising styles

--- js/controller/sales/detailCtrl.js
+++ js/controller/sales/detailCtrl.js
@@ -14,15 +14,29 @@ define([],function(){
         var detailUrl = prefuri+"/product/"+itemID;
 
         /*******通用函数
-         *
-         * @param id
+         *@param data 数组
+         * @param id 查询参数
          * @returns {number}
          */
-         var findIndex = function(id){
+         var findIndex = function(id,data){
             var index = -1;
-            angular.forEach($scope.category.saleNav,function(item,key){
+            angular.forEach(data,function(item,key){
                 if(item.id === id){
                     index = key;
+                }
+            });
+            return index;
+        };
+        /*查询颜色选项
+         *@param data 数组
+         * @param id 查询参数
+         * @returns {number}
+         */
+        var findColorIndex = function(data){
+            var index = -1;
+            angular.forEach(data,function(item,key){
+                if(item.selected){
+                    index = item.id;
                 }
             });
             return index;
@@ -90,17 +104,14 @@ define([],function(){
             }
         };
         /**
-         *
+         *设置选中的颜色
          * @param tags
-         */
-        $scope.selectStyle=function(id,data){
-            var selectCss = "active";
-            $scope.tapColor = findIndex(id,data);
-
+         */////@TODO这边先不用写动作，直接请求数据后渲染
+        $scope.setSelectedColor=function(id){
+            $scope.selectedColorId = id
         };
-        $scope.activeSelect =function(id){
-
-
+        $scope.setSelectedShape=function(id){
+            $scope.selectedShapeId = id
         };
         //初始化http请求+变量
         $http({
@@ -109,10 +120,24 @@ define([],function(){
         }).success(function(data){
             console.log(data);
             $scope.saleDetail = data;
-            //初始化自己设置的变量
+
+            /***初始化自己设置的变量*/
+            //选择商品标签及颜色
+            $scope.itemColors = $scope.saleDetail.attrs[0];//颜色
+            $scope.itemShapes = $scope.saleDetail.attrs[1];//外形
+
+
+            ///@TODO直接请求数据，不用自己写动作
+            $scope.selectedColorId = findColorIndex($scope.itemColors.items);
+            $scope.selectedShapeId = findColorIndex($scope.itemShapes.items);
+
+            console.log('color='+$scope.selectedColorId+'shape'+$scope.selectedShapeId);
+
+            // [选择价格月份]
             $scope.finalMonth = $scope.saleDetail.minMonth;
             $scope.firstTimePay = "0";
-            $scope.calculateMoney = $scope.transferPrice($scope.saleDetail.price)
+            $scope.calculateMoney = $scope.transferPrice($scope.saleDetail.price);
+
 
         }).error(function(){
         });
