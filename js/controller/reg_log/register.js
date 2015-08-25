675ab032fb403708625e2c955c51e00914e7a582
Mrson <rw_Song@sina.com>
2015-08-25 23:41:21 +0800

    Merge remote-tracking branch 'origin/angular' into angular
    Conflicts:
    	js/controller/sales/detailCtrl.js

--- js/controller/reg_log/register.js
+++ js/controller/reg_log/register.js
@@ -1,18 +1,18 @@
 /**
  * Created by slashhuang on 15/8/21.
  */
-define([],function(){
+define([], function () {
     //定义商品分类controller
-    function registerCtrl($scope,$routeParams,$location,$http,$timeout){
+    function registerCtrl($scope, $routeParams, $location, $http, $timeout) {
 
-        var sendSmsUrl = prefuri+"/user/getCode";//发送短信url
-        var registerUrl = prefuri+"/user/register";//提交注册信息
-        $scope.telephone="";
-        $scope.password="";
-        $scope.repeatPassword="";
-        $scope.code="";
+        var sendSmsUrl = prefuri + "/user/getCode";//发送短信url
+        var registerUrl = prefuri + "/user/register";//提交注册信息
+        $scope.telephone = "";
+        $scope.password = "";
+        $scope.repeatPassword = "";
+        $scope.code = "";
 
-        $scope.registerSmsHint="发送验证码";
+        $scope.registerSmsHint = "发送验证码";
 
         //通用函数
         /**
@@ -29,25 +29,26 @@ define([],function(){
          * password.length>=6
          * @param password
          */
-        function checkPws(password){
-            if(password.length>5){
+        function checkPws(password) {
+            if (password.length > 5) {
                 return true
             }
         }
+
         /**
          * 在点击登录时判断是否合法
          * @param mobile
          */
-        $scope.checkMobile = function (mobile,password) {
+        $scope.checkMobile = function (mobile, password) {
             if (!_checkMobile(mobile)) {
-                $scope.checkVaildHint ="手机号格式不符!";
+                $scope.checkVaildHint = "手机号格式不符!";
                 return false;
-            } else{
-                if(!checkPws(password)){
-                    $scope.checkVaildHint ="密码至少6位!";
+            } else {
+                if (!checkPws(password)) {
+                    $scope.checkVaildHint = "密码至少6位!";
                     return false;
                 }
-                else{
+                else {
                     return true;
                 }
             }
@@ -55,30 +56,30 @@ define([],function(){
         /**
          * 点击切换checkbox样式
          */
-        $scope.checkbox_register = function(){
-            $scope.verified=!$scope.verified;
+        $scope.checkbox_register = function () {
+            $scope.verified = !$scope.verified;
         };
         /**
          *
          * @paramjj
          */
-        $scope.countTime =0;
-        var showCountDown = function(){
+        $scope.countTime = 0;
+        var showCountDown = function () {
             //避免多次点击事件
-            if($scope.countTime==0){
-                $scope.countTime=10;
-                $scope.registerSmsHint=$scope.countTime+"秒后重发";
+            if ($scope.countTime == 0) {
+                $scope.countTime = 10;
+                $scope.registerSmsHint = $scope.countTime + "秒后重发";
                 var state = setInterval(function () {
-                    $scope.$apply(function(){
+                    $scope.$apply(function () {
                         $scope.countTime--;
-                        $scope.registerSmsHint=$scope.countTime+"秒后重发";
-                        if($scope.countTime<=0){
-                            $scope.registerSmsHint="发送验证码";
-                            $scope.countTime =0;
+                        $scope.registerSmsHint = $scope.countTime + "秒后重发";
+                        if ($scope.countTime <= 0) {
+                            $scope.registerSmsHint = "发送验证码";
+                            $scope.countTime = 0;
                             clearInterval(state);
                         }
                     })
-                },1000)
+                }, 1000)
             }
 
 
@@ -88,12 +89,12 @@ define([],function(){
          * @param tel
          */
 
-        $scope.sendsms=function(tel){
+        $scope.sendsms = function (tel) {
             //$http({
             //    "method":"post",
             //    "url":""
             //}).success(function(response, status, headers, config){
-                showCountDown();
+            showCountDown();
             //}).error(function(){
             //    showCountDown(10);
             //});
@@ -105,28 +106,48 @@ define([],function(){
          * @param password
          * @param code
          */
-        $scope.register=function(telephone,password,code){
+        $scope.register = function (telephone, password, code) {
             $http({
-                "method":"post",
-                "url":registerUrl,
-                "data":{
-                    "tetephone":telephone,
-                    "password":password,
-                    "code":code
+                "method": "post",
+                "url": registerUrl,
+                "data": {
+                    "tetephone": telephone,
+                    "password": password,
+                    "code": code
                 }
-            }).success(function(response, status, headers, config){
-                $timeout(function(){
+            }).success(function (response, status, headers, config) {
+                $timeout(function () {
                     alert("register succeed")
-                },1000)
+                }, 1000)
                 //@TODO 跳转个人信息
 
 
-            }).error(function(){
-               alert('register failed')
+            }).error(function () {
+                alert('register failed')
             });
         };
+
+        $scope.countTime = 0;
+        var showCountDown = function () {
+            //避免多次点击事件
+            if ($scope.countTime == 0) {
+                $scope.countTime = 10;
+                $scope.registerSmsHint = $scope.countTime + "秒后重发";
+                var state = setInterval(function () {
+                    $scope.$apply(function () {
+                        $scope.countTime--;
+                        $scope.registerSmsHint = $scope.countTime + "秒后重发";
+                        if ($scope.countTime <= 0) {
+                            $scope.registerSmsHint = "发送验证码";
+                            $scope.countTime = 0;
+                            clearInterval(state);
+                        }
+                    });
+                }, 1000);
+            }
+        };
     };
-    registerCtrl.$inject=['$scope','$routeParams','$location','$http','$timeout'];
+    registerCtrl.$inject = ['$scope', '$routeParams', '$location', '$http', '$timeout'];
 
     return registerCtrl;
 });
