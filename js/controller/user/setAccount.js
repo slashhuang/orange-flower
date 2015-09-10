ff4b02302581b55fb4a5787cc50ace0d15c5c2b1
slashhuang <627284482@qq.com>
2015-09-10 18:43:53 +0800

    a little bug

--- js/controller/user/setAccount.js
+++ js/controller/user/setAccount.js
@@ -3,27 +3,28 @@
  */
 define([],function(){
     //定义商品分类controller
-    function setAccountCtrl($scope,$routeParams,$location,$http,$rootScope){
-        var setAccountUrl= $rootScope.prefuri + "/user/complete";
+    function setAccountCtrl($scope,$routeParams,$location,$http,$rootScope) {
+        var setAccountUrl = $rootScope.prefuri + "/user/complete";
         //登出函数
         $scope.logout = function () {
             $http({
-                "method":"post",
-                "url":setAccountUrl
-            }).success(function(data){
+                "method": "post",
+                "url": setAccountUrl
+            }).success(function (data) {
             }).error($rootScope.httpError);
         };
         //重置头像@TODO
-        $scope.changeHead = function(){
+        $scope.changeHead = function () {
 
-        var uData = JSON.parse(localStorage.centerData);
-        if(uData["displayPicture"]){
-            $scope.headPic = $scope.prefuri + "/file/z2/" + uData["displayPicture"];
-        }else{
-            $scope.headPic = "../images/default-head.jpg";
+            var uData = JSON.parse(localStorage.centerData);
+            if (uData["displayPicture"]) {
+                $scope.headPic = $scope.prefuri + "/file/z2/" + uData["displayPicture"];
+            } else {
+                $scope.headPic = "../images/default-head.jpg";
+            }
+            //  显示用户头像
+
         }
-        //  显示用户头像
-
     }
     setAccountCtrl.$inject=['$scope','$routeParams','$location','$http','$rootScope'];
     return setAccountCtrl;
